caa50575bdf49f34700949e3cfe905d1a95e8097
SebastianRodriguez02 <sebastian3696@gmail.com>
2023-01-27 20:54:20 -0500

    nuevo

---
+++ UsoOnChange/assets/js/main.js
@@ -0,0 +1,4 @@
+function seleccionarLenguaje(){
+    var cbxLenguajes = document.getElementById("cbxLenguajes").value;
+    document.getElementById("LenguajeSeleccionado").innerHTML = "Usted ha seleccionado el lenguaje: " + lenguaje;
+}
